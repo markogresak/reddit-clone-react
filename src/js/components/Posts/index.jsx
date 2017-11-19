a28859b544edbabecabcc3c4998bdcc35ea37bb0
Marko Grešak <gresak.marko@gmail.com>
2017-11-19 14:59:07 +0100

    Load more posts on scroll

--- src/js/components/Posts/index.jsx
+++ src/js/components/Posts/index.jsx
@@ -2,10 +2,12 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import styled from 'styled-components';
+import _ from 'lodash';
 
 import {postsListSpacing} from '../../style-vars';
 import PostItem from '../PostItem';
 import {fetchAllPosts} from '../../actions/posts.action';
+import hasScrolledToBottom from '../../helpers/has-scrolled-to-bottom';
 
 const PostsWrapper = styled.div`
   max-width: 960px;
@@ -13,17 +15,54 @@ const PostsWrapper = styled.div`
   padding: ${postsListSpacing}px;
 `;
 
+const pageSize = 20;
+
 class Posts extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      page: 1,
+      maxPage: Math.ceil(props.posts.length / pageSize),
+    };
+  }
+
+  handleScroll = () => {
+    const {page, maxPage} = this.state;
+    if (page < maxPage && hasScrolledToBottom()) {
+      this.setState({page: page + 1});
+    }
+  }
+
   componentWillMount() {
+    window.addEventListener('scroll', this.handleScroll);
     this.props.fetchAllPosts();
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (!_.isEqual(this.props.posts, nextProps.posts)) {
+      this.setState({
+        page: 1,
+        maxPage: Math.ceil(nextProps.posts.length / pageSize),
+      });
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
   renderInner() {
-    if (this.props.arePostsLoading) {
+    const {
+      arePostsLoading,
+      posts,
+    } = this.props;
+
+    if (arePostsLoading) {
       return (<div>Loading...</div>);
     }
-    if (this.props.posts && this.props.posts.length > 0) {
-      return this.props.posts.map(post => <PostItem key={post.id} {...post} />);
+    if (!_.isEmpty(posts)) {
+      return _.take(posts, this.state.page * pageSize).map(post => <PostItem key={post.id} {...post} />);
     }
     return (<div>No posts</div>);
   }
