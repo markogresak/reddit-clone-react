562926296a063ce8b7fe43543ce3e7f831af293f
Marko Grešak <gresak.marko@gmail.com>
2018-02-09 05:41:23 +0100

    Use regex for url domain parsing

--- src/js/helpers/extract-domain.js
+++ src/js/helpers/extract-domain.js
@@ -1,28 +1,7 @@
-function extractHostname(url) {
-  let hostname;
+import _ from 'lodash';
 
-  // find & remove protocol (http, ftp, etc.) and get hostname
-  if (url.indexOf('://') > -1) {
-    hostname = url.split('/')[2];
-  } else {
-    hostname = url.split('/')[0];
-  }
+const urlRe = /:\/\/(.[^/]+)(.*)/;
 
-  // find & remove port number
-  hostname = hostname.split(':')[0];
-  // find & remove "?"
-  hostname = hostname.split('?')[0];
-
-  return hostname;
-}
-
-export default function extractDomain(url) {
-  let domain = extractHostname(url);
-  const splitArr = domain.split('.');
-  const arrLen = splitArr.length;
-
-  if (arrLen > 2) {
-    domain = `${splitArr[arrLen - 2]}.${splitArr[arrLen - 1]}`;
-  }
-  return domain;
+export default function extractDomain(url = '') {
+  return _.get(url.match(urlRe), 1, '');
 }
