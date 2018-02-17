f312654dbe3a4530f4aaccfb99f50b6132ce97c6
Marko Grešak <gresak.marko@gmail.com>
2018-02-17 07:33:15 +0100

    Remove load more on scroll functionality, show all posts on single page

--- src/js/components/Posts/index.jsx
+++ src/js/components/Posts/index.jsx
@@ -3,13 +3,11 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
-import _ from 'lodash';
 
 import {routeCodes} from '../../routes';
 import {postsListSpacing, contentWidth} from '../../style-vars';
 import PostsList from './PostsList';
 import {fetchAllPosts} from '../../actions/posts.action';
-import hasScrolledToBottom from '../../helpers/has-scrolled-to-bottom';
 import urlFromTemplate from '../../helpers/url-from-template';
 import {postTypes} from '../NewPost';
 import {hasUserToken} from '../../helpers/token-manager';
@@ -31,41 +29,9 @@ const NewPostButtonWrapper = styled.div`
   margin-right: 10px;
 `;
 
-const pageSize = 20;
-
 class Posts extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      page: 1,
-      maxPage: Math.ceil(props.posts.length / pageSize),
-    };
-  }
-
-  handleScroll = () => {
-    const {page, maxPage} = this.state;
-    if (page < maxPage && hasScrolledToBottom()) {
-      this.setState({page: page + 1});
-    }
-  }
-
   componentWillMount() {
-    window.addEventListener('scroll', this.handleScroll);
     this.props.fetchAllPosts();
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (!_.isEqual(this.props.posts, nextProps.posts)) {
-      this.setState({
-        page: 1,
-        maxPage: Math.ceil(nextProps.posts.length / pageSize),
-      });
-    }
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
   }
 
   render() {
@@ -88,7 +54,7 @@ class Posts extends Component {
           </NewPostWrapper>
         }
         <PostsWrapper>
-          <PostsList posts={posts} arePostsLoading={arePostsLoading} pageSize={pageSize} page={this.state.page} />
+          <PostsList posts={posts} arePostsLoading={arePostsLoading} />
         </PostsWrapper>
       </div>
     );
